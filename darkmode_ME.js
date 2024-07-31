e7a27eda73edc384c0fd24471f65265fdc969ae3
Muppala Chethan Raju <muppalachethan123@gmail.com>
2024-07-31 23:00:41 +0530

    Dark Mode On/Off

---
+++ darkmode_ME.js
@@ -0,0 +1,17 @@
+var btn = document.getElementById("btn1");
+function myFun() {
+  let btn11 = document.getElementById("btn");
+  btn11.style.visibility = "visible";
+  btn.style.visibility = "hidden";
+  document.body.style.color = "white";
+  document.body.style.backgroundColor = "black";
+}
+
+var btn1 = document.getElementById("btn");
+function myFun1() {
+  let but = document.getElementById("btn1");
+  but.style.visibility = "visible";
+  btn1.style.visibility = "hidden";
+  document.body.style.color = "black";
+  document.body.style.backgroundColor = "white";
+}
